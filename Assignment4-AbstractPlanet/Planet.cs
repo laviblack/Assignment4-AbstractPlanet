766ff29dc7bedb7614882d7ef6174ff01125d649
Lyrica Yoshida <lyoshida@my.centennialcollege.ca>
2017-07-12 18:34:55 -0400

    Created the Abstract Planet class

---
+++ Assignment4-AbstractPlanet/Planet.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Lyrica Yoshida
+ * Date July 12, 2017
+ * Description: This is the Abstract Planet class
+ * Version: 0.1 - Created the Abstract Planet class
+ */
+
+namespace Assignment4_AbstractPlanet
+{
+    /// <summary>
+    /// This is the Abstract Planet class
+    /// </summary>
+    public abstract class Planet
+    {
+        // PRIVATE INSTANCE VARIABLES
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
+
+        // PUBLIC PROPERTIES
+        public double Diameter
+        {
+            get
+            {
+                return this._diameter;
+            }
+        }
+        public double Mass
+        {
+            get
+            {
+                return this._mass;
+            }
+        }
+        public int MoonCount
+        {
+            get
+            {
+                return this._moonCount;
+            }
+            set
+            {
+                this._moonCount = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+        }
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return this._orbitalPeriod;
+            }
+            set
+            {
+                this._orbitalPeriod = value;
+            }
+        }
+        public int RingCount
+        {
+            get
+            {
+                return this._ringCount;
+            }
+            set
+            {
+                this._ringCount = value;
+            }
+        }
+        public double RotationPeriod
+        {
+            get
+            {
+                return this._rotationPeriod;
+            }
+            set
+            {
+                this._rotationPeriod = value;
+            }
+        }
+
+        // CONSTRUCTORS -------------------
+
+            /// <summary>
+            /// This is the main constructor for the Planet class
+            /// It takes three parameters
+            /// </summary>
+            /// <param name="name"></param>
+            /// <param name="diameter"></param>
+            /// <param name="mass"></param>
+        public Planet(string name, double diameter, double mass)
+        {
+            this._name = name;
+            this._diameter = diameter;
+            this._mass = mass;
+        }
+
+        // PRIVATE METHODS
+
+        // PUBLIC METHODS
+        public override string ToString()
+        {
+            return string.Format("Name: {0} Diameter: {1} Mass: {2}", Name, Diameter, Mass);
+        }
+    }
+}
--- Assignment4-AbstractPlanet/Program.cs
+++ Assignment4-AbstractPlanet/Program.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Name: Lyrica Yoshida
+ * Date July 12, 2017
+ * Description: Assignment4
+ * Version: 0.1 - Created Program.cs
+ */
+
 namespace Assignment4_AbstractPlanet
 {
     class Program
