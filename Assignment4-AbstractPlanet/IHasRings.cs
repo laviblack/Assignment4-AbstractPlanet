9afc9677c860f20a90145eb2344e3e3a50dfd589
Lyrica Yoshida <lyoshida@my.centennialcollege.ca>
2017-07-19 12:15:41 -0400

    Created the IHasRings interface

---
+++ Assignment4-AbstractPlanet/IHasRings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Lyrica Yoshida
+ * Date July 19, 2017
+ * Description: This is the IHasRings interface
+ * Version: 0.1 - Created the IHasRings interface
+ */
+
+namespace Assignment4_AbstractPlanet
+{
+    interface IHasRings
+    {        
+        // METHODS --------------
+        bool HasRings();
+
+        // PROPERTIES --------------
+    }
+}
