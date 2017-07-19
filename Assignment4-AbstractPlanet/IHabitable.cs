da064d6705d5cee8269ff31bee63c9bfa6983798
Lyrica Yoshida <lyoshida@my.centennialcollege.ca>
2017-07-19 12:17:53 -0400

    Created the IHabitable interface

---
+++ Assignment4-AbstractPlanet/IHabitable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Lyrica Yoshida
+ * Date July 19, 2017
+ * Description: This is the IHabitable interface
+ * Version: 0.1 - Created the IHabitable interface
+ */
+
+namespace Assignment4_AbstractPlanet
+{
+    interface IHabitable
+    {
+        // METHODS --------------
+        bool Habitalbe();
+
+        // PROPERTIES ---------------
+    }
+}
