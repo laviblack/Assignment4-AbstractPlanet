6cbac28ef6ce07e09780839fbe4f71681418753e
Lyrica Yoshida <lyoshida@my.centennialcollege.ca>
2017-07-19 12:13:46 -0400

    Created the IHasMoons interface

---
+++ Assignment4-AbstractPlanet/IHasMoons.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Lyrica Yoshida
+ * Date July 19, 2017
+ * Description: This is the IHasMoons interface
+ * Version: 0.1 - Created the IHasMoons interface
+ */
+
+namespace Assignment4_AbstractPlanet
+{
+    interface IHasMoons
+    {
+        // METHODS --------------
+        bool HasMoons();
+
+        // PROPERTIES --------------
+    }
+}
